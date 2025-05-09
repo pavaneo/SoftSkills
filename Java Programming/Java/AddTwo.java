747c98854848fbea2724b46e5ed6527d13b39953
Pavan Jain <pavan.kumarj@tyfone.com>
2025-05-09 11:11:28 +0530

    java-dev: Simple Programs updated

---
+++ Java Programming/Java/AddTwo.java
@@ -0,0 +1,9 @@
+public class AddTwo {
+    public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println("\nSum of 10 + 20 = " + sum + "\n");
+    }
+}
