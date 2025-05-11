cae1ba0f4f734110d2ac5a29f6e6070ae88d2e7a
Pavan Jain <pavan.kumarj@tyfone.com>
2025-05-11 15:50:02 +0530

    reactnative-dev: JavaScript folder added

---
+++ ReactNative/AwesomeProject/App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+function App() {
+  return null;
+}
+
+export default App;
